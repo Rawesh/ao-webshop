54b82fea10a8728254c3f86276c9ab1c880cdef7
Rawesh Chedi <99039154@mydavinci.nl>
2018-04-20 11:41:49 +0200

    0 = verwijderen

--- public/js/total.js
+++ public/js/total.js
@@ -6,6 +6,25 @@ var inputPrice = document.getElementsByClassName('price');
 //set total price
 var i;
 
+function changeQuantity(article_id)
+{
+	for (i = 0; i < inputQuantity.length; i++)
+	{
+		console.log(inputQuantity[i].value);
+		if (inputQuantity[i].value < 1)
+		{
+			$.ajax({
+        	type: "GET",
+        	url: '/shoppingcard/delete/' + article_id,
+        	data: "",
+        	success: function() {
+            	alert("verwijdert");
+        	}
+    })
+		}
+	}
+}
+
 function calculateTotal()
 {
 	var total = 0;
